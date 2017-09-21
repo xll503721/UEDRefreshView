6bfa7189399f59de2e2e26481386459b28bdfe22
lizhixuan1 <lizhixuan1@yy.com>
2017-09-21 11:58:54 +0800

    添加例子

---
+++ example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  example
+//
+//  Created by xlL on 9/21/17.
+//  Copyright © 2017 UED. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  example
+//
+//  Created by xlL on 9/21/17.
+//  Copyright © 2017 UED. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
